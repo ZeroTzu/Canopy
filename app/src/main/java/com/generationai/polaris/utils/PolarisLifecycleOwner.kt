9ffb6b4f3e15206896e5b35555db346d6d88e124
ZeroTzu <116237261+ZeroTzu@users.noreply.github.com>
2024-10-07 21:46:41 +0800

    Implementing background service WIP Camera Function (Temporarily removed PolarisLifecycleOwner code)

--- app/src/main/java/com/generationai/polaris/utils/PolarisLifecycleOwner.kt
+++ app/src/main/java/com/generationai/polaris/utils/PolarisLifecycleOwner.kt
@@ -4,16 +4,16 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleRegistry
 
-class PolarisLifecycleOwner : LifecycleOwner {
-    private val lifecycleRegistry = LifecycleRegistry(this)
-    override val lifecycle: Lifecycle
-    override fun getLifecycle(): Lifecycle = lifecycleRegistry
-
-    fun startLifecycle() {
-        lifecycleRegistry.currentState = Lifecycle.State.STARTED
-    }
-
-    fun stopLifecycle() {
-        lifecycleRegistry.currentState = Lifecycle.State.DESTROYED
-    }
-}
+//class PolarisLifecycleOwner : LifecycleOwner {
+////    private val lifecycleRegistry = LifecycleRegistry(this)
+////    override val lifecycle: Lifecycle
+////    override fun getLifecycle(): Lifecycle = lifecycleRegistry
+////
+////    fun startLifecycle() {
+////        lifecycleRegistry.currentState = Lifecycle.State.STARTED
+////    }
+////
+////    fun stopLifecycle() {
+////        lifecycleRegistry.currentState = Lifecycle.State.DESTROYED
+////    }
+//}
