f00b29c8e586e2f296290b66d183d09765a6d93a
ZeroTzu <116237261+ZeroTzu@users.noreply.github.com>
2024-12-22 14:27:04 +0800

    Created a basic indoor map view for the home page

---
+++ app/src/main/java/com/generationai/polaris/ui/CustomCircleButtonLayout.kt
@@ -0,0 +1,27 @@
+package com.generationai.polaris.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import android.widget.TextView
+import com.generationai.polaris.R
+import com.google.android.material.button.MaterialButton
+
+class CustomCircleButtonLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+    init {
+        inflate(context, R.layout.custom_circle_button_layout, this)
+    }
+
+    fun setIcon(iconRes: Int) {
+        findViewById<MaterialButton>(R.id.customButton_materialButton).setIconResource(iconRes)
+    }
+
+    fun setText(text: String) {
+        findViewById<TextView>(R.id.customButton_textView).text = text
+    }
+
+}
