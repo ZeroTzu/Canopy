66ad4b2aed42416718d5af7e5eb2a7fffe9aa9cc
ZeroTzu <116237261+ZeroTzu@users.noreply.github.com>
2024-12-28 17:15:50 +0800

    Revamped Tap in Fragment with tap in fragment alt

--- app/src/main/java/com/teamname/canopy/FragmentTapInAlt.kt
+++ app/src/main/java/com/teamname/canopy/FragmentTapInAlt.kt
@@ -1,22 +1,25 @@
 package com.teamname.canopy
 
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.transition.TransitionManager
 import android.util.Log
 import android.view.HapticFeedbackConstants
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.FieldValue
-import com.google.firebase.firestore.SetOptions
+import com.teamname.canopy.databinding.FragmentTapInAltBinding
 import com.teamname.canopy.databinding.FragmentTapInBinding
 import kotlin.random.Random
-import kotlin.time.Duration.Companion.hours
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,37 +28,36 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [TapInFragment.newInstance] factory method to
+ * Use the [FragmentTapInAlt.newInstance] factory method to
  * create an instance of this fragment.
  */
-class TapInFragment : Fragment() {
+class FragmentTapInAlt : Fragment() {
+    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var binding: FragmentTapInAltBinding
     private var handler = Handler(Looper.getMainLooper())
     private var isNFCActive = false
     private lateinit var viewModel: MainActivityViewModel
-    //binding
-    private lateinit var binding: FragmentTapInBinding
-    private var buttonAnimationRunnable = object : Runnable {
-        override fun run() {
-            val imageView = requireActivity().findViewById<View>(R.id.fragment_tap_in_animation_imageView)
 
-            imageView.animate().scaleX(4f).scaleY(4f).alpha(0f).setDuration(1000)
-                .withEndAction {
-                    imageView.animate().scaleX(1f).scaleY(1f).alpha(1f)
-                }
-            performHaptic(imageView)
-            handler.postDelayed(this,1500)
+
+    private var showPopupRunnable = object : Runnable {
+        override fun run() {
+            val textView = binding.fragmentTapInAltAnimationTextView
+
+            performHaptic(textView)
+            animateWelcomeText(true)
         }
     }
-    private var addVolunteerHistoryRunnable = object : Runnable {
+    private var hidePopupRunnable = object : Runnable {
         override fun run() {
-            addVolunteerHistory(Timestamp.now())
-            handler.removeCallbacks(buttonAnimationRunnable)
-            isNFCActive = false
+            val textView = binding.fragmentTapInAltAnimationTextView
 
+            performHaptic(textView)
+            animateWelcomeText(false)
         }
     }
+
     fun addVolunteerHistory(currentTimestamp: Timestamp) {
         val activity = (requireActivity() as MainActivity)
         activity.firestore.collection("users").document(activity.firebaseAuth.currentUser!!.uid).get().addOnSuccessListener { result ->
@@ -110,14 +112,17 @@ class TapInFragment : Fragment() {
                             .update("pointsEarned", FieldValue.increment(pointsEarned.toLong()))
                     }
                     .addOnSuccessListener {
+                        binding.fragmentTapInAltAnimationTextView.text = "Successfully Checked Out!"
+                        handler.post(showPopupRunnable)
+                        handler.postDelayed(hidePopupRunnable,5000)
                         Log.i("CanopyTapInFragment", "Successfully updated tap-out details for session $index")
-                        val snackBar = Snackbar.make(requireView(), "Successfully checked out!", Snackbar.LENGTH_LONG)
-                        snackBar.show()
+
                     }
                     .addOnFailureListener { e ->
-                        Log.e("CanopyTapInFragment", "Error updating tap-out details", e)
+
                         val snackBar = Snackbar.make(requireView(), "Error checking out", Snackbar.LENGTH_LONG)
                         snackBar.show()
+                        Log.e("CanopyTapInFragment", "Error updating tap-out details", e)
                     }
             } else {
                 Log.i("CanopyTapInFragment", "No session found to update. Adding a new session.")
@@ -137,11 +142,13 @@ class TapInFragment : Fragment() {
                     .addOnSuccessListener {
                         val canopyName= viewModel.getCanopyById(newCanopyId)?.canopyName
                         if (canopyName != null) {
-                            val snackBar = Snackbar.make(requireView(), "Welcome to ${viewModel.getCanopyById(newCanopyId)?.canopyName}", Snackbar.LENGTH_LONG)
-                            snackBar.show()
+                            binding.fragmentTapInAltAnimationTextView.text = "Welcome to ${viewModel.getCanopyById(newCanopyId)?.canopyName}"
+                            handler.post(showPopupRunnable)
+                            handler.postDelayed(hidePopupRunnable,5000)
                         }else{
-                            val snackBar = Snackbar.make(requireView(), "Welcome!", Snackbar.LENGTH_LONG)
-                            snackBar.show()
+                            binding.fragmentTapInAltAnimationTextView.text = "Welcome!"
+                            handler.post(showPopupRunnable)
+                            handler.postDelayed(hidePopupRunnable,5000)
                         }
                         Log.i("CanopyTapInFragment", "Successfully added a new session")
                     }
@@ -153,13 +160,67 @@ class TapInFragment : Fragment() {
         }
     }
 
+    private fun animateWelcomeText(show: Boolean) {
+        // Reference the root layout
+        val constraintLayout = binding.root as ConstraintLayout
+
+        // Animate changes with TransitionManager
+        TransitionManager.beginDelayedTransition(constraintLayout)
+
+        if (show) {
+            // Make the TextView visible and ensure constraints are updated
+            binding.fragmentTapInAltAnimationTextView.apply {
+                visibility = View.VISIBLE
+                alpha = 0f
+                translationY = 50f
+                animate()
+                    .alpha(1f)
+                    .translationY(0f)
+                    .setDuration(1000)
+                    .start()
+            }
+        } else {
+            // Update layout to handle the removal smoothly
+            binding.fragmentTapInAltAnimationTextView.apply {
+                animate()
+                    .alpha(0f)
+                    .translationY(50f)
+                    .setDuration(1000)
+                    .withEndAction {
+                        // Hide the TextView after animation
+                        visibility = View.GONE
+
+                        // Trigger a layout pass to adjust the position of other views
+                        TransitionManager.beginDelayedTransition(constraintLayout)
+                    }
+                    .start()
+            }
+        }
+    }
+
+
+
+    private var addVolunteerHistoryRunnable = object : Runnable {
+        override fun run() {
+            addVolunteerHistory(Timestamp.now())
+            isNFCActive = false
+
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-        viewModel = ViewModelProvider(requireActivity())[MainActivityViewModel::class.java]
+        binding = FragmentTapInAltBinding.inflate(layoutInflater)
+        viewModel= ViewModelProvider(requireActivity())[MainActivityViewModel::class.java]
+        val constraintLayout = binding.root
+        val animationDrawable = constraintLayout.background as AnimationDrawable
+        animationDrawable.setEnterFadeDuration(2500)
+        animationDrawable.setExitFadeDuration(5000)
+        animationDrawable.start()
     }
 
     override fun onCreateView(
@@ -167,26 +228,24 @@ class TapInFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentTapInBinding.inflate(inflater, container, false)
 
-        binding.fragmentTapInMaterialButton.setOnClickListener {
+        binding.fragmentTapInAltDebugButton.setOnClickListener {
             if (!isNFCActive) {
-                handler.postDelayed(addVolunteerHistoryRunnable,3000)
+                handler.postDelayed(addVolunteerHistoryRunnable,500)
                 isNFCActive = true
-                handler.post(buttonAnimationRunnable)
             }else{
                 isNFCActive = false
-                handler.removeCallbacks(buttonAnimationRunnable)
                 handler.removeCallbacks(addVolunteerHistoryRunnable)
             }
         }
-        binding.fragmentTapInCloseButton.setOnClickListener {
+        binding.fragmentTapInAltCloseButton.setOnClickListener {
             var fragmentTransaction = parentFragmentManager.beginTransaction()
             fragmentTransaction.replace(R.id.nav_host_fragment, Home())
             fragmentTransaction.commit()
         }
         return binding.root
     }
+
     fun performHaptic(view: View){
         val successful = view.isActivated
         when (successful){
@@ -195,9 +254,18 @@ class TapInFragment : Fragment() {
         }
     }
     companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment FragmentTapInAlt.
+         */
+        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            TapInFragment().apply {
+            FragmentTapInAlt().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
